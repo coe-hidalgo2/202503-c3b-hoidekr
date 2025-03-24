c888b136f3258652106d44148050efc1d4f34dd3
Jan Hoidekr <64287197+hoidekr@users.noreply.github.com>
2025-03-24 11:10:42 +0100

    ok Update devcontainer.json

---
+++ _deps/bmpi3-subbuild/CMakeFiles/bmpi3-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmpi3-populate"
+  "CMakeFiles/bmpi3-populate-complete"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-build"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-configure"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-download"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-install"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-mkdir"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-patch"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-test"
+  "bmpi3-populate-prefix/src/bmpi3-populate-stamp/bmpi3-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/bmpi3-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/CMakeFiles/test_mpi3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_mpi3.dir/test_mpi3.cpp.o"
+  "CMakeFiles/test_mpi3.dir/test_mpi3.cpp.o.d"
+  "test_mpi3"
+  "test_mpi3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_mpi3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
